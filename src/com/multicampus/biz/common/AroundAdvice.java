8a7b9093a27e845b846fc265d51260666dafee8f
JungYoungsu <terun707@gmail.com>
2018-12-11 16:18:55 +0900

    !!! AOP : XML -> ANNO

--- src/com/multicampus/biz/common/AroundAdvice.java
+++ src/com/multicampus/biz/common/AroundAdvice.java
@@ -1,9 +1,18 @@
 package com.multicampus.biz.common;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Service;
 
+@Service
+@Aspect	// Aspect = PointCut + Advice
 public class AroundAdvice {
+	@Pointcut("execution(* com.multicampus.biz..*Impl.*(..))")
+	public void allPointcut() {} // 호출되지 않는 메소드
 
+	@Around("allPointcut()")
 	public Object aroundLog(ProceedingJoinPoint jp) throws Throwable { 
 		String method = jp.getSignature().getName(); // 메소드 이름
 		// String longstr = jp.getSignature().toLongString(); // 리턴값 패키지 클래스명 메소드 전체 정보
--- src/com/multicampus/biz/common/LogAdvice.java
+++ src/com/multicampus/biz/common/LogAdvice.java
@@ -1,9 +1,18 @@
 package com.multicampus.biz.common;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Service;
 
+@Service
+@Aspect
 public class LogAdvice {
-
+	@Pointcut("execution(* com.multicampus.biz..*Impl.*(..))")
+	public void allPointcut() {}
+	
+	@Before("allPointcut()")
 	public void printLog(JoinPoint jp) {
 		String method = jp.getSignature().getName();
 		Object[] args = jp.getArgs();
